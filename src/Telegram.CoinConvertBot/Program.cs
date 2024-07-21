9ff4c90499a0611338dfe6735eef07ddf69ac064
rodyyyds <xiaotian3577@gmail.com>
2024-07-21 14:10:45 -0300

    Update Program.cs

--- src/Telegram.CoinConvertBot/Program.cs
+++ src/Telegram.CoinConvertBot/Program.cs
@@ -103,7 +103,8 @@ static void ConfigureServices(HostBuilderContext Context, IServiceCollection Ser
         botClient.SetMyCommandsAsync(new BotCommand[]
         {
         new BotCommand(){Command="start",Description="开始使用"},  
-        new BotCommand(){Command="qiand",Description="一键签到"},        
+        new BotCommand(){Command="qiand",Description="一键签到"},  
+        new BotCommand(){Command="jifen",Description="兑换奖品"},       
         }).GetAwaiter().GetResult();
     }
     Log.Logger.Information("Telegram机器人上线！机器人ID：{Id}({username})，机器人名字：{FirstName}.", me.Id, $"@{me.Username}", me.FirstName);
